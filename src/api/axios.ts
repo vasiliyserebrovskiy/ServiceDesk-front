60f9aae5785c2ec4084608e76b16c199e70e718a
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2026-05-14 15:10:34 +0200

    change errors flow. separate credentials and active/bloc user logic

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -18,11 +18,8 @@ let failedQueue: QueueItem[] = [];
 
 const processQueue = (error: unknown) => {
   failedQueue.forEach((promise) => {
-    if (error) {
-      promise.reject(error);
-    } else {
-      promise.resolve();
-    }
+    if (error) promise.reject(error);
+    else promise.resolve();
   });
 
   failedQueue = [];
@@ -34,59 +31,71 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
+    const status = error.response?.status;
+    const message = error.response?.data?.message;
+
     const isAuthRequest =
       originalRequest.url?.includes("/auth/login") ||
       originalRequest.url?.includes("/auth/refresh-token");
 
-    // if the access token has expired
-    if (
-      error.response?.status === 401 &&
-      !originalRequest._retry &&
-      !isAuthRequest
-    ) {
+    // =========================
+    // 401 → refresh flow
+    // =========================
+    if (status === 401 && !originalRequest._retry && !isAuthRequest) {
       originalRequest._retry = true;
 
-      // if refresh is already underway, we put it in the queue.
       if (isRefreshing) {
         return new Promise((resolve, reject) => {
           failedQueue.push({ resolve, reject });
-        }).then(() => {
-          return api(originalRequest);
-        });
+        }).then(() => api(originalRequest));
       }
 
       isRefreshing = true;
 
       try {
-        // refresh using cookie
         await api.post("/v1/auth/refresh-token");
-
         processQueue(null);
-
-        // repeating the original request
         return api(originalRequest);
       } catch (err) {
         processQueue(err);
-
-        // if refresh is also dead → logout
         store.dispatch(clearAuth());
-
         return Promise.reject(err);
       } finally {
         isRefreshing = false;
       }
     }
 
-    // all error messages
-    const skipToast = error.config?.meta?.skipToast;
-    if (!isAuthRequest && !skipToast) {
-      const message =
-        error.response?.data?.message ||
-        error.message ||
-        "Something went wrong";
+    // =========================
+    // 403 → AUTH STATE errors
+    // (blocked / inactive)
+    // =========================
+    if (status === 403) {
+      const isAuthStateError =
+        message?.includes("not active") || message?.includes("locked");
 
-      store.dispatch(setError(message));
+      if (isAuthStateError) {
+        store.dispatch(clearAuth());
+        store.dispatch(setError(message)); // here we need banner
+        return Promise.reject(error);
+      }
     }
+
+    // =========================
+    // LOGIN PAGE errors (401)
+    // did not otuch → to avoid breaking the login UI
+    // =========================
+
+    // =========================
+    // GLOBAL errors (toast)
+    // =========================
+    const skipToast = error.config?.meta?.skipToast;
+
+    if (!isAuthRequest && !skipToast) {
+      const finalMessage = message || error.message || "Something went wrong";
+
+      store.dispatch(setError(finalMessage));
+    }
+
     return Promise.reject(error);
   },
 );
